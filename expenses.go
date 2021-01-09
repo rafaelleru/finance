da207b433c0f29f3f82419c0a9b2a6feb4018cba
Rafa leyva <rafaelleru95103@gmail.com>
2021-01-09 17:57:27 +0100

    Add functionality to git track new expenses

--- expenses.go
+++ expenses.go
@@ -22,11 +22,32 @@ func transaction_to_line(tr Transaction) string {
 	return fmt.Sprintf("%s\t%.2f\t%s\t%s\n", tr.id, tr.value, date_string, tr.description)
 }
 
+func commit_transaction(description string) int {
+	os.Chdir("/home/rl/.expenses/")
+	cmd := exec.Command("git", "add", "expenses.txt")
+	_, err := cmd.CombinedOutput()
+
+	if err != nil {
+		fmt.Print(err)
+		return -1
+	}
+
+	cmd = exec.Command("git", "commit", "-m", "Tracks : \"" + description + "\"")
+	_, err = cmd.CombinedOutput()
+
+	if err != nil {
+		fmt.Print(err)
+		return -1
+	}
+
+	return 0
+}
+
 func main() {
 
 	transactions_file := os.Getenv("EXPENSES_FILE") 
 	if len(transactions_file) == 0 {
-		transactions_file = "~/.expenses/expenses.txt"
+		transactions_file = "/home/rl/.expenses/expenses.txt"
 	}
 
 	var addFlag = flag.Bool("add", false, "Create a new transaction")
@@ -51,10 +72,19 @@ func main() {
 		file, err := os.OpenFile(transactions_file, os.O_WRONLY|os.O_APPEND, 0644)
 
 		if err != nil {
-			fmt.Println("Error writing to file")
+			fmt.Println("Error opening " + transactions_file)
+			fmt.Println(err)
+			os.Exit(-1)
 		}
 
 		defer file.Close()
 		file.WriteString(string(transaction_line))
+
+		status := commit_transaction(tr.description)
+
+		if status == -1 {
+			fmt.Println("Error tracking the new transaction in git")
+			os.Exit(-1)
+		}
 	}
 }
