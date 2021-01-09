b6c9169d55744268a7dd1e4254ae64659e80d8df
Rafa leyva <rafaelleru95103@gmail.com>
2021-01-09 17:28:13 +0100

    Finish functionality to add transactions

--- expenses.go
+++ expenses.go
@@ -1,5 +1,7 @@
 package main
 
+import "os"
+import "os/exec"
 import "crypto/md5"
 import "fmt"
 import "time"
@@ -13,19 +15,39 @@ type Transaction struct {
 	description string
 }
 
+func transaction_to_line(tr Transaction) string {
+	var date_string = fmt.Sprintf("%d-%02d-%02dT%02d:%02d",
+									tr.date.Year(), tr.date.Month(), tr.date.Day(),
+									tr.date.Hour(), tr.date.Minute())
+	return fmt.Sprintf("%s\t%.2f\t%s\t%s\n", tr.id, tr.value, date_string, tr.description)
+}
+
 func main() {
+
+	transactions_file := os.Getenv("EXPENSES_FILE") 
+	if len(transactions_file) == 0 {
+		transactions_file = "~/.expenses/expenses.txt"
+	}
+
 	var addFlag = flag.Bool("add", false, "Create a new transaction")
 	var valueFlag = flag.Float64("value", 0.0, "Value for the transaction")
 	var mFlag = flag.String("m", "", "Description for the transaction")
 
 	flag.Parse()
 
-
 	if *addFlag == true {
+		fmt.Println("Adding new transaction")
 		var tr = Transaction{value: *valueFlag, description: *mFlag, date: time.Now()}
-		tr.id = fmt.Sprintf("%x",  md5.Sum([]byte(tr.description)))
+		tr.id = fmt.Sprintf("%x",  md5.Sum([]byte(tr.date.String())))
 
+		var transaction_line = []byte(transaction_to_line(tr))
+		file, err := os.OpenFile(transactions_file, os.O_WRONLY|os.O_APPEND, 0644)
+
+		if err != nil {
+			fmt.Println("Error writing to file")
+		}
+
+		defer file.Close()
+		file.WriteString(string(transaction_line))
 	}
-
-
 }
